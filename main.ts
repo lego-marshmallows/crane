d9789fbe6215cde384af86c7041a7c2771242ad1
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-12-14 15:28:33 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-
+function goback () {
+    motors.largeBC.steer(-20, 50, -1400, MoveUnit.Degrees)
+}
+function turnandrelease () {
+    motors.mediumA.run(20, 55, MoveUnit.Degrees)
+    pause(1000)
+}
+function goToTheCrane () {
+    motors.largeBC.steer(-12, 50, 1720, MoveUnit.Degrees)
+    motors.largeBC.run(-10, 150, MoveUnit.Degrees)
+}
+goToTheCrane()
+turnandrelease()
+goback()
+brick.exitProgram()
